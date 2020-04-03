15b77e00033b6688f7a7a40673ff7dfc928df803
Jagpreet2108 <jagpreetkaur2108@gmail.com>
2020-04-03 15:01:32 +0530

    CommitOne

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace Question1.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public double Contact { get; set; }
+    }
+}
